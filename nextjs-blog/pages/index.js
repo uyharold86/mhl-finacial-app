bcf0f5ea43436ff3c9f93c6c8e5003dd1b1acd6c
uyharold86 <>
2021-04-05 13:25:31 +0000

    added MLH Finance logo and colors

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,81 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>MLH Finance</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300&display=swap" rel="stylesheet"/> 
+      </Head>
+
+      <main>
+       <header>
+        <h1>
+          <span className="header-title-red">M</span>
+          <span className="header-title-yellow">L</span>
+          <span className="header-title-blue">H</span> Finance
+        </h1>
+       </header>
+      </main>
+
+      
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0;
+          
+        }
+
+        main {
+          width: 100%;
+          height: 120px;
+          padding: 5rem 0;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          padding 0 10px;
+          text-align: center; 
+        }
+
+        .header-title-red {
+          color: #e73427;
+        }  
+
+        .header-title-blue {
+          color: #1d539f;
+        }     
+
+        .header-title-yellow {
+          color: #f8b92a;
+        } 
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: 'Teko', sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
